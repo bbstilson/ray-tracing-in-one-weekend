58acff7f9bb752b13021bf2d310fcc7b87dc2593
Brandon Stilson <bbstilson@fastmail.com>
2022-02-28 21:45:49 -0800

    movable camera

--- src/camera.rs
+++ src/camera.rs
@@ -10,19 +10,28 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(v_fov: i32, aspect_ratio: f64) -> Camera {
+    pub fn new(
+        look_from: Point3d,
+        look_at: Point3d,
+        view_up: Vector3,
+        v_fov: i32,
+        aspect_ratio: f64,
+    ) -> Camera {
         let theta = Camera::degrees_to_radians(v_fov);
         let h = (theta / 2.0).tan();
         let viewport_height = h * 2.0;
         let viewport_width = aspect_ratio * viewport_height;
 
+        let w = (look_from - look_at).unit();
+        let u = view_up.cross(w).unit();
+        let v = w.cross(u);
+
         let focal_length = 1.0;
 
-        let origin = Vector3::zero();
-        let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vector3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - (horizontal / 2.0) - (vertical / 2.0) - Vector3::new(0.0, 0.0, focal_length);
+        let origin = look_from;
+        let horizontal = u * viewport_width;
+        let vertical = v * viewport_height;
+        let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - w;
 
         Camera {
             origin,
@@ -32,10 +41,10 @@ impl Camera {
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + (self.horizontal * u) + (self.vertical * v) - self.origin,
+            self.lower_left_corner + (self.horizontal * s) + (self.vertical * t) - self.origin,
         )
     }
 
